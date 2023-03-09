0de20644975fe42019fb0e649156443a2abde077
David Brown <ndbrown6@users.noreply.github.com>
2023-03-09 17:48:52 -0500

    Update cellularFraction.R

--- R/cellularFraction.R
+++ R/cellularFraction.R
@@ -1,5 +1,5 @@
 'cellularFraction' <- function (f_hat, qt, sq)
 {
-	alpha = signif(2*(f_hat)/(sq-(f_hat)*(qt-2)), 2)
+	alpha = signif( (2*f_hat)/(sq + (2*f_hat - qt*f_hat)), 3)
 	return(invisible(alpha))
 }
